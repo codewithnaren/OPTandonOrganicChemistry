a730a79c14b7b26388b64b42ae2187774e3fc762
codewithnaren <91406174+codewithnaren@users.noreply.github.com>
2021-10-15 10:34:07 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const url = 'https://codewithnaren.github.io/OPTandonOrganicChemistry/Polity6.pdf';
+const url = 'https://codewithnaren.github.io/OPTandonOrganicChemistry/OPTandonOrganicChemistry.pdf';
 
 
 let pdfDoc = null,
